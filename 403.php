c99f1d7414704e12e524f74a439dfc0c3a46e031
maki <47719596+maki64@users.noreply.github.com>
2020-01-22 03:50:37 -0500

    Update 403.php

--- 403.php
+++ 403.php
@@ -4,7 +4,7 @@
 <body style="background-image:url(/static/system.gif)">
 <center><img height=480 width=640 src="/static/403.jpg"/>
 <marquee scrollamount="40"><h1><p style="font-family: sans-serif; font-size:30px; color: black;">awwwwwwwwwwwwwww shit</p></h1></marquee>
-<p style="color: red;background:black">403</p>
+<p style="color: blue;background:green">403</p>
 <br /> <br />
 <param name="movie" value="/static/congrats.swf">
 </center>
